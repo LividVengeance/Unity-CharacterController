28af5296c36a594f29bce53181eb906be8869ad9
Andrew <ajbarnes97@gmail.com>
2021-08-21 12:11:39 +1200

    Basic Moving Platform Setup

---
+++ Assets/Scenes/Project Two/MovingPlatform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour, IMoverController
+{
+    public PhysicsMover mover;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mover.MoverController = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// Used to tell what the characters position and rotations should be
+    public void UpdateMovement(out Vector3 goalPosition, out Quaternion goalRotation, float deltaTime)
+    {
+        goalPosition = Vector3.zero;
+        goalRotation = Quaternion.identity;
+    }
+}
